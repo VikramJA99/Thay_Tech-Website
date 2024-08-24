f4b345e87b555a677e8738a11284d95f9d21fdd7
VikramJA99 <johnvickyvikram@gmail.com>
2024-08-24 23:14:24 +0530

    Change Cloud support folder and add images
    Signed-off-by: VikramJA99 <johnvickyvikram@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
-import Navbar from './components/navbar/Navbar';
-import HomePage from './components/home/HomePage';
-import Footer from './components/footer/Footer';
-import TweetCardComponent from './components/home/TweetCardComponent';
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import HomePage from "./components/home/HomePage";
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ApplicationSupport from "./components/service/cloud/ApplicationSupport";
+import DatabaseSupport from "./components/service/cloud/DatabaseSupport";
+import OSSupport from "./components/service/cloud/OSSupport";
+import NetworkSupport from "./components/service/cloud/NetworkSupport";
+import StaffOutSourcing from "./components/service/outsourcing/StaffOutsourcing";
 const App: React.FC = () => {
   return (
     <div>
-      <Navbar />
-      <main>
-        <HomePage />
-        <TweetCardComponent />
-      </main>
-      <Footer />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="*" element={<HomePage />} />
+          <Route path="/ApplicationSupport" element={<ApplicationSupport />} />
+          <Route path="/DatabaseSupport" element={<DatabaseSupport />} />
+          <Route path="/OSSupport" element={<OSSupport />} />
+          <Route path="/NetworkSupport" element={<NetworkSupport />} />
+          <Route path="/StaffOutSourcing" element={<StaffOutSourcing />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import Carousel from './Carousel';
-import ClientLogos from './ClientLogos'
-import AccordionImage from './AccordionImage'
+import React from "react";
+import Carousel from "./Carousel";
+import ClientLogos from "./ClientLogos";
+import AccordionImage from "./AccordionImage";
 
 const HomePage: React.FC = () => {
   return (
     <div className="homepage">
       <Carousel />
       <ClientLogos />
-      <AccordionImage/>
+      <AccordionImage />
     </div>
   );
 };
